f7d366223f7e15a944614dd353f3be8aab0ab765
Henrique <lira.apren@gmail.com>
2020-11-21 14:50:21 -0200

    2111202013h50m

---
+++ statics/JS/carrosel.js
@@ -0,0 +1,50 @@
+var timer;
+
+$(function(){
+    $('.slide img:eq(0)').addClass("ativo").show();
+    var texto= $(".ativo").attr("alt");
+    $('.slide').prepend("");
+
+
+slide();
+
+function slide(){
+    var $NEXT= $();
+    var $ATUAL= $('.ativo');
+    if($('.ativo').next().length){
+        $NEXT =$('.ativo').next();
+    }
+     else{
+         $NEXT =$('.slide img:eq(0)');
+         }
+    timer =$ATUAL.data('timer')
+
+
+setTimeout(function() {
+
+$ATUAL.fadeOut().removeClass("ativo");
+
+$NEXT.fadeIn().addClass("ativo");
+
+
+var texto
+= $NEXT.attr('alt');
+
+$('.slide p').hide().html(texto).delay(0.5).fadeIn();
+
+
+slide();
+
+}, timer);
+
+}
+
+});
+
+function botoes(){
+
+
+
+ 
+
+};
