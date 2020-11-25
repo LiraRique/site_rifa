694f7e311c7400df196177719e7012dd187c7822
unknown <guilherme.2010.guilherme@gmail.com>
2020-11-24 22:35:28 -0300

    241120202235

---
+++ views.py
@@ -0,0 +1,80 @@
+#################################### APLICAÇÃO FLASK #####################################
+
+from flask import Flask, render_template, request, redirect, session, flash, url_for, send_from_directory
+import hashlib
+'''
+import mysql.connector
+'''
+from datetime import date, datetime
+
+
+app = Flask(__name__)
+app.secret_key = 'alura'
+#################################### ^^^^^^^^^^^^^^^^^^ ####################################
+
+
+#################################### CONEXÃO SQL SERVER ####################################
+'''
+banco = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="rifa"
+    )
+    
+db = banco.cursor()
+
+'''
+#################################### ^^^^^^^^^^^^^^^^^^ ####################################
+
+####################################        VIEWS ADMINISTRADOR       ######################
+
+@app.route('/')
+def pagina_rifa():
+    proxima = request.args.get('proxima')
+    return render_template('pagina_rifa.html')
+
+
+@app.route('/formulario', methods=['POST', 'GET'])
+def formulario():
+    proxima = request.args.get('proxima')
+    return render_template('formulario.html')
+
+
+#################################### ^^^^^^^^^^^^^^^^^^ ####################################
+
+#################################### AUTENTICAÇÃO ##########################################
+'''
+@app.route('/autenticar', methods=['POST', ])
+def autenticar():
+    usuario = login_usuario.buscar_por_id(request.form['email_usuario'])
+    password = request.form['senha_usuario']
+    password_app = hashlib.md5(password.encode())
+    senha_aplicacao = password_app.hexdigest()
+    if usuario:
+        if usuario.senha_aplicacao == senha_aplicacao:
+                session['usuario_logado'] = usuario.email_usuario
+                flash('Administrador(a)  ' + usuario.nome_usuario + ' logado!')
+                proxima_pagina = url_for('deash_usuario')
+                return redirect(proxima_pagina)
+        else:
+            flash('Senha invalida, tente denovo!')
+            return redirect(url_for('login'))
+    else:
+        flash('Usuario não encontrado!')
+        return redirect(url_for('login'))
+
+
+
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Deslogado com sucesso!')
+    return redirect(url_for('login'))
+
+
+
+'''
+#################################### ^^^^^^^^^^^^^^^^^^ ####################################
+
+app.run(debug=True)
